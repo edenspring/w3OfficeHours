017a56b6aba28f0e39f44d6f001152e8f286ef2d
Bill Adams <vikingbill@gmail.com>
2021-09-12 14:35:21 -0400

    recursive solve

--- 02-sort.js
+++ 02-sort.js
@@ -17,7 +17,34 @@
                     'Rlain',
                     'Renarin',
                  ]
-                 
+
 const bridge4Sorted = nameSorter(bridge4) // ['Dabbid', 'Kaladin', 'Lopen', 'Moash', 'Renarin', 'Rlain', 'Rock', 'Sigzil', 'Skar', 'Teft']
 
  * ***********************************************************/
+
+const bridge4 = [
+    'Kaladin',
+    'Teft',
+    'Lopen',
+    'Dabbid',
+    'Rock',
+    'Sigzil',
+    'Skar',
+    'Moash',
+    'Rlain',
+    'Renarin',
+ ]
+
+const nameSorter = (arr, sorted = []) => {
+    if (!arr.length) return sorted
+    const lowest = arr.reduce((accum, ele) => {
+        if (accum < ele) return accum
+        return ele
+    })
+    const idx = arr.indexOf(lowest)
+    sorted.push(lowest)
+    arr.splice(idx, 1)
+    return nameSorter(arr, sorted)
+}
+
+console.log(nameSorter(bridge4))
