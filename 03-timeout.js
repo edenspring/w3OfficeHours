bda6ae9147c772ebce060a2c7da5a9573182f203
Bill Adams <vikingbill@gmail.com>
2021-09-12 14:44:54 -0400

    fix missing '

--- 03-timeout.js
+++ 03-timeout.js
@@ -11,31 +11,6 @@ South = 125ms
 East = 300ms
 West = 175ms
 
-const directions = ['North', 'East', 'East', 'West', 'North', 'South, 'South','West']
-
-* ***********************************************************/
 const directions = ['North', 'East', 'East', 'West', 'North', 'South', 'South','West']
 
-const navigator = (directions) => {
-    if (!directions.length) return
-    const current = directions.shift()
-    let delay
-    switch (current) {
-        case 'North':
-            delay = 250
-            break
-        case 'South':
-            delay = 125
-            break
-        case 'East':
-            delay = 300
-            break
-        case 'West':
-            delay = 175
-            break
-    }
-    console.log(`Moving ${current} and waiting ${delay}ms`)
-    setTimeout(navigator, delay, directions)
-}
-
-navigator(directions)
+* ***********************************************************/
