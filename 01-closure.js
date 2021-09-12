8b684d2e0d6ddea3f9921830e614e7ca0f7a04f0
Bill Adams <vikingbill@gmail.com>
2021-09-12 14:30:55 -0400

    solve 01 two ways

--- 01-closure.js
+++ 01-closure.js
@@ -3,10 +3,11 @@
  will return  another function that accepts an array and splices 
  in the word 'spliced' from 0 to the indicated index (inclusive)
  and returns the result. The word 'spliced' will go between the
- original entries.
+ original entries. This function may or may not mutate the
+ original array.
 
  hint : think about how this will change the indices we are
- working with.
+ working with if you do spluce 
 
  hint : there are many ways to solve this problem, you _do not_
  need to use the splice method.
@@ -18,3 +19,27 @@ console.log(result) // [1, 'spliced', 2, 'spliced', 3, 'spliced', 4, 'spliced', 
 
 
  * ***********************************************************/
+
+// const splicer = (num) => (arr) => {
+//     const res = []
+//     for (let i = 0; i < arr.length; i++){
+//         res.push(arr[i])
+//         if (i <= num){
+//             res.push('spliced')
+//         }
+//     }
+//     return res;
+// }
+
+const splicer = (num) => (arr) => {
+    let idx = 1
+    let count = 0
+    while (count <= num){
+        arr.splice(idx, 0, 'spliced')
+        idx += 2
+        count++
+    }
+    return arr
+}
+
+console.log(splicer(3)([1,2,3,4,5,6]))
