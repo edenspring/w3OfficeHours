4dfa1d5d5eadb886b7cc230a149a12d2995058f2
Bill Adams <vikingbill@gmail.com>
2021-09-12 14:19:30 -0400

    add 02

---
+++ 02-sort.js
@@ -0,0 +1,23 @@
+/**************************************************************
+ Write a function called nameSorter that accepts an array of names
+ and returns a new array of those same names but in alphabetical order.
+ You don't have to use recursion to solve this, but you might benefit 
+ from doing so. Make sure you give yourself a default parameter called
+ sorted when building this function
+
+ const bridge4 = [
+                    'Kaladin',
+                    'Teft,
+                    'Lopen',
+                    'Dabbid',
+                    'Rock',
+                    'Sigzil',
+                    'Skar',
+                    'Moash',
+                    'Rlain',
+                    'Renarin',
+                 ]
+                 
+const bridge4Sorted = nameSorter(bridge4) // ['Dabbid', 'Kaladin', 'Lopen', 'Moash', 'Renarin', 'Rlain', 'Rock', 'Sigzil', 'Skar', 'Teft']
+
+ * ***********************************************************/
