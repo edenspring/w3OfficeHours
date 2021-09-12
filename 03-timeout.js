86f8ae7281c6ff9efd339c27009fa2568cca51d3
Bill Adams <vikingbill@gmail.com>
2021-09-12 14:22:49 -0400

    add 03

---
+++ 03-timeout.js
@@ -0,0 +1,13 @@
+/**************************************************************
+Write a function called navigator that takes in an array of
+directions, prints the direction, then waits a specified amount
+of time before moving to the next one.
+
+North = 250ms
+South = 125ms
+East = 300ms
+West = 175ms
+
+const directions = ['North', 'East', 'East', 'West', 'North', 'South, 'South','West']
+
+ * ***********************************************************/
